567013c57dfda305f1ed31535d3fad12cbfd49d5
Bethany <no1isnothing@gmail.com>
2020-01-20 16:45:53 -0600

    Initial commit. Set up intro sample file and a few pages

---
+++ myscript.js
@@ -0,0 +1,14 @@
+function init()
+{
+    alert("JS is set up correctly");
+    //This is were we can set up variables to be used throughout the project
+    localStorage.seeds = 0;
+    localStorage.turns = 0;
+
+}
+
+//TODO Add param. Add event listener to button
+function loadPage()
+{
+    window.location.href = "page1.html";
+}
